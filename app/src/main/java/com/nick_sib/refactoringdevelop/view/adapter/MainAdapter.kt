06f52f6b4c064811bbd90caee8fcd58384d956eb
nick-sib <khc1981@mail.ru>
2021-02-14 01:33:20 +0700

    added some UX

--- app/src/main/java/com/nick_sib/refactoringdevelop/view/adapter/MainAdapter.kt
+++ app/src/main/java/com/nick_sib/refactoringdevelop/view/adapter/MainAdapter.kt
@@ -3,37 +3,53 @@ package com.nick_sib.refactoringdevelop.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ExpandableListView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.nick_sib.refactoringdevelop.R
 import com.nick_sib.refactoringdevelop.model.data.DataModel
 
 class MainAdapter(
-    val clickListener: ((String) -> Unit)? = null
-): RecyclerView.Adapter<MainAdapter.RecyclerItemViewHolder>() {
+val clickListener: ((String) -> Unit)? = null
+): RecyclerView.Adapter<BaseViewHolder>() {
+
+    private val SEARCH_RESUT_IS_EMPTY = 0
+    private val SEARCH_RESUT_SIMPLY = 1
 
     var data: List<DataModel> = emptyList()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
+    set(value) {
+        field = if (value.isEmpty())
+            listOf(DataModel(null, null))
+        else
+            value
+        notifyDataSetChanged()
+    }
+
+    override fun getItemViewType(position: Int): Int =
+        if ((data.size == 1) && (data[0].text == null) && (data[0].meanings.isNullOrEmpty()))
+            SEARCH_RESUT_IS_EMPTY
+        else
+            SEARCH_RESUT_SIMPLY
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return  if (viewType == SEARCH_RESUT_IS_EMPTY) {
+            RecyclerEmptySearchHolder(inflater
+                .inflate(R.layout.recyclerview_item_empty_search_result, parent, false))
+        } else {
+            RecyclerItemViewHolder(inflater
+                .inflate(R.layout.recyclerview_item_search_result, parent, false))
         }
+    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerItemViewHolder =
-        RecyclerItemViewHolder(LayoutInflater.from(parent.context)
-            .inflate(R.layout.recyclerview_item_search_result, parent, false))
-
-
-    override fun onBindViewHolder(holder: RecyclerItemViewHolder, position: Int) {
-        holder.bind(data.get(position))
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        holder.bind(data[position])
     }
 
     override fun getItemCount(): Int =
-            data.size
+        data.size
 
-
-    inner class RecyclerItemViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        fun bind(data: DataModel) {
+    inner class RecyclerItemViewHolder(view: View): BaseViewHolder(view) {
+        override fun bind(data: DataModel) {
             if (layoutPosition != RecyclerView.NO_POSITION) {
                 itemView.findViewById<TextView>(R.id.tv_header).text = data.text ?: ""
                 itemView.findViewById<TextView>(R.id.tv_description).text =
@@ -44,6 +60,10 @@ class MainAdapter(
                 }
             }
         }
+    }
+
+    inner class RecyclerEmptySearchHolder(view: View): BaseViewHolder(view) {
+        override fun bind(data: DataModel) {}
     }
 
 }
