21d3f6c33bd6300f0237ae3c4ccb1a57e6fb81a0
nick-sib <khc1981@mail.ru>
2021-02-20 23:24:19 +0700

    deleted unused classes

--- app/src/main/java/com/nick_sib/refactoringdevelop/view/MainActivity.kt
+++ app/src/main/java/com/nick_sib/refactoringdevelop/view/MainActivity.kt
@@ -1,24 +1,15 @@
 package com.nick_sib.refactoringdevelop.view
 
-
 import android.os.Bundle
 import android.view.View
-import android.widget.FrameLayout
-import android.widget.ProgressBar
 import android.widget.Toast
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.nick_sib.refactoringdevelop.R
 import com.nick_sib.refactoringdevelop.databinding.ActivityMainBinding
 import com.nick_sib.refactoringdevelop.model.data.AppState
-import com.nick_sib.refactoringdevelop.presenter.IPresenter
-import com.nick_sib.refactoringdevelop.presenter.MainPresenterImpl
 import com.nick_sib.refactoringdevelop.view.adapter.MainAdapter
 import com.nick_sib.refactoringdevelop.view.base.BaseActivity
-import com.nick_sib.refactoringdevelop.view.base.IView
 import com.nick_sib.refactoringdevelop.view.fragment.SearchDialogFragment
 import com.nick_sib.refactoringdevelop.view.main.MainInteractor
 import com.nick_sib.refactoringdevelop.view.main.MainViewModel
@@ -29,93 +20,78 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
 
     @Inject
     internal lateinit var viewModelFactory: ViewModelProvider.Factory
-
     override lateinit var model: MainViewModel
-//    private val adapter: MainAdapter by lazy { MainAdapter(onListItemClickListener) }
-    private val fabClickListener: View.OnClickListener =
-        View.OnClickListener {
-            val searchDialogFragment = SearchDialogFragment.instance()
-//            searchDialogFragment.setOnSearchClickListener(onSearchClickListener)
-            searchDialogFragment.show(supportFragmentManager, BOTTOM_SHEET_FRAGMENT_DIALOG_TAG)
-        }
-//    private val onListItemClickListener: MainAdapter.OnListItemClickListener =
-//        object : MainAdapter.OnListItemClickListener {
-//            override fun onItemClick(data: DataModel) {
-//                Toast.makeText(this@MainActivity, data.text, Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//    private val onSearchClickListener: SearchDialogFragment.OnSearchClickListener =
-//        object : SearchDialogFragment.OnSearchClickListener {
-//            override fun onClick(searchWord: String) {
-//                isNetworkAvailable = isOnline(applicationContext)
-//                if (isNetworkAvailable) {
-//                    model.getData(searchWord, isNetworkAvailable)
-//                } else {
-//                    showNoInternetConnectionDialog()
-//                }
-//            }
-//        }
+
+    private lateinit var binding: ActivityMainBinding
+    private val loadDialog: View by lazy {
+        binding.root.findViewById(R.id.v_load_dialog)
+    }
+    private lateinit var adapter: MainAdapter
+    private var errorSnack: Snackbar? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         model = viewModelFactory.create(MainViewModel::class.java)
-        model.subscribe().observe(this@MainActivity, Observer<AppState> { renderData(it) })
+        model.subscribe().observe(this@MainActivity) { renderData(it) }
 
-        findViewById<FloatingActionButton>(R.id.search_fab).setOnClickListener(fabClickListener)
-//        findViewById<RecyclerView>(R.id.main_activity_recyclerview).layoutManager = LinearLayoutManager(applicationContext)
-//        findViewById<RecyclerView>(R.id.main_activity_recyclerview).adapter = adapter
+        binding.searchFab.setOnClickListener {
+            val searchDialogFragment = SearchDialogFragment.instance()
+            searchDialogFragment.setOnSearchClickListener {
+                model.getData(it, true)
+            }
+            searchDialogFragment.show(supportFragmentManager, BOTTOM_SHEET_FRAGMENT_DIALOG_TAG)
+        }
+        adapter = MainAdapter {
+            Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+        }
+        binding.rvSearchResult.adapter = adapter
     }
 
-    override fun renderData(appState: AppState) {
-        when (appState) {
+    override fun renderData(dataModel: AppState) {
+        when (dataModel) {
             is AppState.Success -> {
-                showViewWorking()
-                val data = appState.data
-                if (data.isNullOrEmpty()) {
-                    showAlertDialog(
-                        getString(R.string.dialog_tittle_sorry),
-                        getString(R.string.empty_server_response_on_success)
-                    )
-                } else {
-//                    adapter.setData(data)
-                }
+                hideLoadingDialog()
+                hideErrorDialog()
+                adapter.data = dataModel.data ?: emptyList()
             }
+            is AppState.Error -> { showErrorDialog()}
             is AppState.Loading -> {
-                showViewLoading()
-                if (appState.progress != null) {
-//                    findViewById<ProgressBar>(R.id.progress_bar_horizontal).apply {
-//                        visibility = View.VISIBLE
-//                        progress = appState.progress
-//                    }
-//                    findViewById<ProgressBar>(R.id.progress_bar_round).visibility = View.GONE
-//                    progress_bar_horizontal.
-                } else {
-//                    findViewById<ProgressBar>(R.id.progress_bar_horizontal).visibility = View.GONE
-//                    findViewById<ProgressBar>(R.id.progress_bar_round).visibility = View.VISIBLE
-                }
-            }
-            is AppState.Error -> {
-                showViewWorking()
-//                showAlertDialog(getString(R.string.error_stub), appState.error.message)
+                hideErrorDialog()
+                showLoadingIndocator()
             }
         }
     }
 
-    private fun showViewWorking() {
-//        findViewById<FrameLayout>(R.id.loading_frame_layout).visibility = View.GONE
+    private fun hideLoadingDialog(){
+        loadDialog.visibility = View.GONE
+        binding.searchFab.visibility = View.VISIBLE
     }
 
-    private fun showViewLoading() {
-//        findViewById<FrameLayout>(R.id.loading_frame_layout).visibility = View.VISIBLE
+    private fun hideErrorDialog(){
+        errorSnack?.dismiss()
+    }
+
+    private fun showLoadingIndocator(){
+        loadDialog.visibility = View.VISIBLE
+        binding.searchFab.visibility = View.GONE
+    }
+
+    private fun showErrorDialog(){
+        errorSnack = Snackbar.make(binding.root, "Что - пошло не так", Snackbar.LENGTH_INDEFINITE)
+            .setAction("Прервать") {
+                hideLoadingDialog()
+
+            }.also {
+                it.show()
+            }
     }
 
     companion object {
-        private const val BOTTOM_SHEET_FRAGMENT_DIALOG_TAG = "74a54328-5d62-46bf-ab6b-cbf5fgt0-092395"
+        private const val BOTTOM_SHEET_FRAGMENT_DIALOG_TAG = "75b10273-329d-41ef-a433-bd69e20d0f06"
     }
 }
